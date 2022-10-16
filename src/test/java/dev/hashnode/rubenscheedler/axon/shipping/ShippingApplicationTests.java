3e17e16efc38364f29ad9c7ae88aa41f44674b7c
ruben <scheedler.r@tkppensioen.nl>
2022-10-16 09:48:18 +0200

    Add Spring initialzr code

---
+++ src/test/java/dev/hashnode/rubenscheedler/axon/shipping/ShippingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hashnode.rubenscheedler.axon.shipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
